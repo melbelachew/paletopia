4a599db5dcc45bc33141f8504f13a785e88c86e7
Mel Belachew <melbelachew@gmail.com>
2019-03-27 14:13:26 -0400

    updated api key

--- assets/js/getColors.js
+++ assets/js/getColors.js
@@ -19,7 +19,8 @@ var getColors = (url) => {
     //api request to google vision. This returns the response as visionResponse
     var visionOption = JSON.stringify({ "requests": [{ "image": { "source": { "imageUri": url } }, "features": [{ "type": 'IMAGE_PROPERTIES', "maxResults": 5 }] }] });
     var visionResponse = new XMLHttpRequest;
-    visionResponse.open("POST", "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyA9EHeI2lrYJnjfEMhI0rU-J8yyfAOSOAs", !0);
+    var mykey = config.MY_KEY;
+    visionResponse.open("POST", "https://vision.googleapis.com/v1/images:annotate?key="+mykey , !0);
     visionResponse.send(visionOption)
 console.log(visionResponse);
     //doing stuff with the response once the response is loaded
